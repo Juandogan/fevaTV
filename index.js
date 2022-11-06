52a29a5bdc8b1339340aaedb7f2d80f4e09eef20
juandogan <juandogan@gmail.com>
2022-11-06 18:34:35 -0300

    manifesto

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.set('port', process.env.PORT || 3000);  // tomo app e nsu propiedad .set  //
 app.use(morgan('dev')); // morgan es una funcion, la pegamos en la propiedad use de app. y pasamos el parametro dev que indica que mostrara el mensaje por consola de desarrollo. 
 app.use(express.json()); // habilita para que el servidor entienda formato json, es una propiedad de la dependencia Express.npom
 
-app.use(cors('http://127.0.0.1:8080'));
+app.use(cors('https://fevatv.com.ar'));
 app.use(bodyParser.json({limit: '200mb'}));
 app.use(bodyParser.urlencoded({limit: '200mb', extended: true}));
 
@@ -46,7 +46,7 @@ app.post('/upload', multiPartMiddleware, (req,res)=>{
     console.log(req.files['archivos'].path) 
     var link = req.files['archivos'].path
       
-var url = 'https://www.fevatv.com.ar/upload/'+ link.slice(8) 
+var url = 'https://fevatv.com.ar/upload/'+ link.slice(8) 
 console.log({'url': url })
     res.json({'url':url });
     
@@ -57,7 +57,7 @@ app.post('/upload2', multiPartMiddleware, (req,res)=>{
    
     var link = req.files['upload'].path
       
-var url = 'https://www.fevatv.com.ar/upload/'+ link.slice(8) 
+var url = 'https://fevatv.com.ar/upload/'+ link.slice(8) 
 console.log({'url': url })
     res.json({'url':url });
     
